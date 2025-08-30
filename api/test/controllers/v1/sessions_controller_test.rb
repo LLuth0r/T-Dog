0a071b3dc3d11e139cd7130247fbc1db500ade72
Mike Pakulak <mike.pakulak@gmail.com>
2025-08-30 10:38:03 -0400

    changing _url to _path in sessions_test

--- api/test/controllers/v1/sessions_controller_test.rb
+++ api/test/controllers/v1/sessions_controller_test.rb
@@ -8,7 +8,7 @@ class V1::SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "create" do
-    post v1_sessions_url, params: { court_id: @court.id, user_contact: "you@example.com" }, as: :json
+    post v1_sessions_path, params: { court_id: @court.id, user_contact: "you@example.com" }, as: :json
     assert_response :success
     body = JSON.parse(response.body)
     assert body["id"]
@@ -16,20 +16,20 @@ class V1::SessionsControllerTest < ActionDispatch::IntegrationTest
 
   test "stop" do
     s = Session.create!(court: @court, user_contact: "x", status: "active", started_at: Time.current)
-    post stop_v1_session_url(s.id)
+    post stop_v1_session_path(s.id)
     assert_response :success
     assert_equal "processing", s.reload.status
   end
 
   test "show" do
     s = Session.create!(court: @court, user_contact: "x", status: "active", started_at: Time.current)
-    get v1_session_url(s.id)
+    get v1_session_path(s.id)
     assert_response :success
   end
 
   test "presigned_download" do
     s = Session.create!(court: @court, user_contact: "x", status: "delivered", started_at: Time.current, s3_key: "sessions/#{SecureRandom.hex}/clip.mp4")
-    get presigned_download_v1_session_url(s.id)
+    get presigned_download_v1_session_path(s.id)
     assert_response :success
     body = JSON.parse(response.body)
     assert_includes body["url"], "amazonaws.com"
